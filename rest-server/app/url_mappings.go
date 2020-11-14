911503f48b01db0058b0092c8bc182df272c083f
Steven DeVere <sdevere@xogrp.com>
2020-11-13 17:47:57 -0800

    Creates side project endpoints

--- rest-server/app/url_mappings.go
+++ rest-server/app/url_mappings.go
@@ -7,9 +7,17 @@ import (
 func mapUrls() {
 	router.GET("ping", controllers.Ping)
 
+	// article endpoints
 	router.GET("/articles", controllers.GetAllArticles)
 	router.GET("/articles/:id", controllers.GetArticle)
 	router.POST("/articles", controllers.CreateArticle)
 	router.PUT("/articles", controllers.UpdateArticle)
 	router.DELETE("/articles/:id", controllers.DeleteArticle)
+
+	// project endpoints
+	router.GET("/projects", controllers.GetAllSideProjects)
+	router.GET("/projects/:id", controllers.GetSideProject)
+	router.POST("/projects", controllers.CreateSideProject)
+	router.PUT("/projects", controllers.UpdateSideProject)
+	router.DELETE("/projects/:id", controllers.DeleteSideProject)
 }
